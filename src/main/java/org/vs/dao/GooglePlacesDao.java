4f9f74b34343a7aae85136ef52dd67f2383fc9a6
Vishal Sinha <vishal.sinha21@gmail.com>
2016-01-31 11:09:14 +0530

    authkey as variable and fix alignment on weather page

--- src/main/java/org/vs/dao/GooglePlacesDao.java
+++ src/main/java/org/vs/dao/GooglePlacesDao.java
@@ -9,26 +9,28 @@ import org.springframework.web.client.RestTemplate;
 public class GooglePlacesDao {
 
     private RestTemplate restTemplate;
+    
+    private String authkey = "authkey";
 
     public GooglePlacesDao() {
         this.restTemplate = new RestTemplate();
     }
 
     public JsonNode getRestaurants(String latitude, String longitude) {
-        String template = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s,%s&radius=1000&types=restaurant&key=authkey";
-        String url = String.format(template, latitude, longitude);
+        String template = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s,%s&radius=1000&types=restaurant&key=%s";
+        String url = String.format(template, latitude, longitude, authkey);
         return restTemplate.getForObject(url, JsonNode.class);
     }
 
     public JsonNode getRestaurantDetails(String placeId) {
-        String template = "https://maps.googleapis.com/maps/api/place/details/json?placeid=%s&key=authkey";
-        String url = String.format(template, placeId);
+        String template = "https://maps.googleapis.com/maps/api/place/details/json?placeid=%s&key=%s";
+        String url = String.format(template, placeId, authkey);
         return restTemplate.getForObject(url, JsonNode.class);
     }
 
     public String getPhoto(String photoRef) {
-        String template = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=%s&key=authkey";
-        String url = String.format(template, photoRef);
+        String template = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=%s&key=%s";
+        String url = String.format(template, photoRef, authkey);
         return restTemplate.getForObject(url, String.class);
     }
 
